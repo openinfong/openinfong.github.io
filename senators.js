0fbf5266065ca461c8f2c7315f884f085d9d63d3
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-11-23 22:58:57 +0100

    Create senators.js

---
+++ senators.js
@@ -0,0 +1,551 @@
+const senateContacts = [
+{
+id: 1,
+name: "Sen. Godswill Akpabio",
+position: "Senate President",
+department: "Akwa Ibom North-West",
+phone: "",
+email: "",
+office: "",
+state: "akwa-ibom",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senator_akpabio" },
+{ platform: "twitter", url: "https://x.com/SenGodswill" },
+{ platform: "facebook", url: "https://www.facebook.com/senakpabio" },
+{ platform: "website", url: "https://godswillakpabio.com/" }
+],
+source: "https://nass.gov.ng/mps/single/185"
+},
+{
+id: 2,
+name: "Sen. Ibrahim Jibrin",
+position: "Deputy Senate President",
+department: "Kano North",
+phone: "",
+email: "ibrahim.jibrin@nass.gov.ng",
+office: "",
+state: "kano",
+socials: [
+{ platform: "twitter", url: "https://x.com/barauijibrin" },
+{ platform: "facebook", url: "https://www.facebook.com/barauij" },
+{ platform: "instagram", url: "https://www.instagram.com/senatorbaraujibrin" }
+],
+source: "https://nass.gov.ng/mps/single/433"
+},
+{
+id: 3,
+name: "Sen. Orji Kalu",
+position: "Senator",
+department: "Abia North",
+phone: "+2348034000001",
+email: "okalu@orjikalu.com",
+office: "onu ibina square, igbere, bende lga, abia state",
+state: "abia",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/oukgram" },
+{ platform: "twitter", url: "https://x.com/OUKtweets" },
+{ platform: "facebook", url: "https://www.facebook.com/orjiuzorkalu01" },
+{ platform: "website", url: "https://orjikalumediacentre.com/" }
+],
+source: "https://nass.gov.ng/mps/single/97"
+},
+{
+id: 4,
+name: "Sen. Augustine Akobundu",
+position: "Senator",
+department: "Abia Central",
+phone: "+2348033169941",
+email: "aoakobundu@senatenass.gov.ng",
+office: "no 7a factory road, umuahia, abia state",
+state: "abia",
+socials: [],
+source: "https://nass.gov.ng/mps/single/694"
+},
+{
+id: 5,
+name: "Sen. Enyinnaya Abaribe",
+position: "Senator",
+department: "Abia South",
+email: "enyiabaribey@yahoo.com",
+office: "147 market road aba.",
+state: "abia",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/senatorenyiabaribe/" },
+{ platform: "twitter", url: "https://x.com/senatorabaribe" }
+],
+source: "https://nass.gov.ng/mps/single/318"
+},
+{
+id: 6,
+name: "Sen. Aminu Abbas",
+position: "Senator",
+department: "Adamawa Central",
+phone: "",
+email: "iyaabbas@gmail.com",
+office: "",
+state: "adamawa",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/aminu_iyaabbas/" }
+],
+source: "https://nass.gov.ng/mps/single/544"
+},
+{
+id: 7,
+name: "Sen. Binos Yaroe",
+position: "Senator",
+department: "Adamawa South",
+phone: "+2348034050460",
+email: "bdyaroe@gmail.com",
+office: "makwada street, opposite makwada square, numan, adamawa state",
+state: "adamawa",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/p/Binos-Dauda-Yaroe-for-Senate-Adamawa-Southern-Senatorial-Zone-100057884209753/" },
+{ platform: "website", url: "https://senatorbinos.com/" }
+],
+source: "https://nass.gov.ng/mps/single/87"
+},
+{
+id: 8,
+name: "Sen. Ishaku Abbo",
+position: "Senator",
+department: "Adamawa North",
+phone: "+2348066285112",
+email: "Faradugun@gmail.com",
+office: "senator ishaku abbo (sia) senatorial office mubi north adamawa state",
+state: "adamawa",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senatorishakuabbo/" },
+{ platform: "facebook", url: "https://www.facebook.com/senatorishakuabbo/" }
+],
+source: "https://nass.gov.ng/mps/single/303"
+},
+{
+id: 9,
+name: "Sen. Bassey Etim",
+position: "Senator",
+department: "Akwa Ibom North-East",
+phone: "",
+email: "anigembassey@gmail.com",
+office: "",
+state: "akwa-ibom",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/SenatorAniekanBassey/" }
+],
+source: "https://nass.gov.ng/mps/single/591"
+},
+{
+id: 10,
+name: "Sen. Victor Umeh",
+position: "Senator",
+department: "Anambra Central",
+phone: "",
+email: "victorimeh1962@yahoo.com",
+office: "",
+state: "anambra",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/senatorvictorumeh/" },
+{ platform: "twitter", url: "https://x.com/SenVictorUmeh" }
+],
+source: "https://nass.gov.ng/mps/single/603"
+},
+{
+id: 11,
+name: "Sen. Abdul Ningi",
+position: "Senator",
+department: "Bauchi Central",
+phone: "",
+email: "abdulahmed.ningi@outlook.com",
+office: "",
+state: "bauchi",
+socials: [
+{ platform: "twitter", url: "https://x.com/sen_abdulningi" },
+{ platform: "instagram", url: "https://www.instagram.com/sen.abdulningi" }
+],
+source: "https://nass.gov.ng/mps/single/567"
+},
+{
+id: 12,
+name: "Sen. Samaila Kaila",
+position: "Senator",
+department: "Bauchi North",
+phone: "",
+email: "samailadk@gmail.com",
+office: "",
+state: "bauchi",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/sen_dr.samaila_dahuwa/" }
+],
+source: "https://nass.gov.ng/mps/single/651"
+},
+{
+id: 13,
+name: "Sen. Umar Buba",
+position: "Senator",
+department: "Bauchi South",
+phone: "",
+email: "shehububa2015@yahoo.com",
+office: "",
+state: "bauchi",
+socials: [
+{ platform: "facebook", url: "https://web.facebook.com/honsbu/" }
+],
+source: "https://nass.gov.ng/mps/single/652"
+},
+{
+id: 14,
+name: "Sen. Benson Agadaga",
+position: "Senator",
+department: "Bayelsa East",
+phone: "",
+email: "bensonagadaga@gmail.com",
+office: "",
+state: "bayelsa",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/p/SenatorBenson-Agadaga-100064772408680/" }
+],
+source: "https://nass.gov.ng/mps/single/577"
+},
+{
+id: 15,
+name: "Sen. Seriake Dickson",
+position: "Senator",
+department: "Bayelsa West",
+phone: "+2348056085520",
+email: "dickson.seriake01@gmail.com",
+office: "Suite 3.09 Senate New Building,  National Assembly Complex, Abuja, Nigeria",
+state: "bayelsa",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/IamHSDickson/" },
+{ platform: "instagram", url: "https://www.instagram.com/hsdickson/" },
+{ platform: "website", url: "https://www.henryseriakedickson.com/" }
+],
+source: "https://nass.gov.ng/mps/single/493"
+},
+{
+id: 16,
+name: "Sen. Abba Moro",
+position: "Senator",
+department: "Benue South",
+phone: "+2348068870606",
+email: "abahmoro@yahoo.com",
+office: "opposite dolphin hotel, g.r.a otukpo, benue state",
+state: "benue",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/moroabba" }
+],
+source: "https://nass.gov.ng/mps/single/356"
+},
+{
+id: 17,
+name: "Sen. Kaka Lawan",
+position: "Senator",
+department: "Borno Central",
+phone: "",
+email: "kakashehulawan@gmail.com",
+office: "",
+state: "borno",
+socials: [
+{ platform: "facebook", url: "https://web.facebook.com/p/Senator-Kaka-Shehu-Lawan-Update-100063953506071/" }
+],
+source: "https://nass.gov.ng/mps/single/587"
+},
+{
+id: 18,
+name: "Sen. Mohammed Ndume",
+position: "Senator",
+department: "Borno South",
+phone: "+2348109480004",
+email: "mohammed.ndume@nass.gov.ng",
+office: "off gombe road, adjacent to excellenct academy, mustapha aliyu stadium road, biu",
+state: "borno",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/p/Sen-Muhammad-Ali-Ndume-100044306895406/" }
+],
+source: "https://nass.gov.ng/mps/single/412"
+},
+{
+id: 19,
+name: "Sen. Mohammed Monguno",
+position: "Senator",
+department: "Borno North",
+phone: "",
+email: "mtmonguno@gmail.com",
+office: "",
+state: "borno",
+socials: [],
+source: "https://nass.gov.ng/mps/single/528"
+},
+{
+id: 20,
+name: "Sen. Jarigbe Jarigbe",
+position: "Senator",
+department: "Cross River North",
+phone: "",
+email: "contact@senatorjarigbeagom.com",
+office: "",
+state: "cross-river",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/jarigbeagom/" },
+{ platform: "facebook", url: "https://www.facebook.com/jarigbe.agom.7/" },
+{ platform: "twitter", url: "https://x.com/JJarigbe" },
+{ platform: "website", url: "https://senatorjarigbeagom.com/" },
+],
+source: "https://nass.gov.ng/mps/single/451"
+},
+{
+id: 21,
+name: "Sen. Williams Jonah",
+position: "Senator",
+department: "Cross River Central",
+phone: "",
+email: "eteng_j@yahoo.com",
+office: "",
+state: "cross-river",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/p/Senator-Eteng-Jonah-Williams-100063646243204/" }
+],
+source: "https://nass.gov.ng/mps/single/557"
+},
+{
+id: 22,
+name: "Sen. Ned Nwoko",
+position: "Senator",
+department: "Delta North",
+phone: "+2348037227010",
+email: "princenednwoko@yahoo.com",
+office: "Mount Ned Resort, Idumuje-Ugboko, Aniocha North L.G.A , Delta State",
+state: "delta",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/princenednwoko" },
+{ platform: "twitter", url: "https://x.com/Prince_NedNwoko" },
+{ platform: "facebook", url: "https://www.facebook.com/officialprincened/" },
+{ platform: "website", url: "https://sennednwoko.com.ng/" }
+],
+source: "https://nass.gov.ng/mps/single/600"
+},
+{
+id: 23,
+name: "Sen. Michael Bamidele",
+position: "Senator",
+department: "Ekiti Central",
+phone: "+23480911112",
+email: "amicusng@gmail.com",
+office: "ikere road, ado ekiti, ekiti state",
+state: "ekiti",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senatormichaelopeyemibamidele" },
+{ platform: "twitter", url: "https://x.com/SenatorOpeyemi" },
+{ platform: "facebook", url: "https://www.facebook.com/senatoropeyemibamidele/" }
+],
+source: "https://nass.gov.ng/mps/single/158"
+},
+{
+id: 24,
+name: "Sen. Kelvin Chukwu",
+position: "Senator",
+department: "Enugu East",
+phone: "",
+email: "kelvinchukwu22ng@gmail.com",
+office: "",
+state: "enugu",
+socials:[
+{ platform: "facebook", url: "https://www.facebook.com/p/Senator-Sir-Kelvin-Chukwu-61551769017268/" }
+],
+source: "https://nass.gov.ng/mps/single/518"
+},
+{
+id: 25,
+name: "Sen. Danjuma Mohammed",
+position: "Senator",
+department: "Gombe Central",
+phone: "+2347068686699",
+email: "mdgoje1@gmail.com",
+office: "township road, kumo, akko lga, gombe state",
+state: "gombe",
+socials: [
+{ platform: "twitter", url: "https://x.com/mdgoje1" }
+],
+source: "https://nass.gov.ng/mps/single/408"
+},
+{
+id: 26,
+name: "Sen. Anthony Yaro",
+position: "Senator",
+department: "Gombe South",
+phone: "",
+email: "anthonyyaro1@gmail.com",
+office: "",
+state: "gombe",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/HonSiyakoYaro/" },
+{ platform: "twitter", url: "https://x.com/HonSiyakoYaro" }
+],
+source: "https://nass.gov.ng/mps/single/605"
+},
+{
+id: 27,
+name: "Sen. Ibrahim Dankwambo",
+position: "Senator",
+department: "Gombe North",
+phone: "",
+email: "dankwanbo@yahoo.com",
+office: "",
+state: "gombe",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/HassanIbrahimDankwambo/" },
+{ platform: "twitter", url: "https://x.com/HEDankwambo" }
+],
+source: "https://nass.gov.ng/mps/single/679"
+},
+{
+id: 28,
+name: "Sen. Ezenwa Onyewuchi",
+position: "Senator",
+department: "Imo East",
+phone: "+2348032012132",
+email: "ezeonyewuchi@gmail.com",
+office: "Plot C13 Opposite Chief Emmanuel Iwuanyanwu Glass House Amawire Orji Imo State",
+state: "imo",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senatoronyewuchimedia/" },
+{ platform: "website", url: "https://ezenwaonyewuchi.org/" }
+],
+source: "https://nass.gov.ng/mps/single/679"
+},
+{
+id: 29,
+name: "Sen. Babangida Hussaini",
+position: "Senator",
+department: "Jigawa North-West",
+phone: "",
+email: "babangidahussaini22@gmail.com",
+office: "",
+state: "jigawa",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senatorwali/" },
+{ platform: "facebook", url: "https://www.facebook.com/senatorwali/" },
+{ platform: "twitter", url: "https://x.com/senatorwali" }
+],
+source: "https://nass.gov.ng/mps/single/533"
+},
+{
+id: 30,
+name: "Sen. Mallam-Madori Abdulhamid",
+position: "Senator",
+department: "Jigawa North-East",
+phone: "",
+email: "aannadori@gmail.com",
+office: "",
+state: "jigawa",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/amb_sen_ahmad_abdulhamid_media/" }
+],
+source: "https://nass.gov.ng/mps/single/639"
+},
+{
+id: 31,
+name: "Sen. Sunday Katung",
+position: "Senator",
+department: "Kaduna South",
+phone: "",
+email: "sunnyside1914@gmail.com",
+office: "",
+state: "kaduna",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/smkatung/" },
+{ platform: "facebook", url: "https://www.facebook.com/SMKatung/" },
+{ platform: "twitter", url: "https://x.com/smkatung" }
+],
+source: "https://nass.gov.ng/mps/single/576"
+},
+{
+id: 32,
+name: "Sen. Lawal Usman",
+position: "Senator",
+department: "Kaduna Central",
+phone: "",
+email: "mrla300@gmail.com",
+office: "",
+state: "kaduna",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/mrla7411/" },
+{ platform: "facebook", url: "https://www.facebook.com/mrla2023/" },
+{ platform: "twitter", url: "https://x.com/mrla7411" }
+],
+source: "https://nass.gov.ng/mps/single/663"
+},
+{
+id: 33,
+name: "Sen. Nasiru Sani Zangon Daura",
+position: "Senator",
+department: "Katsina North",
+phone: "",
+email: "nazdaura@yahoo.com",
+office: "",
+state: "katsina",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senzangondauramedia/" },
+{ platform: "facebook", url: "https://www.facebook.com/magajinbaba2019/" },
+{ platform: "twitter", url: "https://x.com/NasiruSZD" }
+],
+source: "https://nass.gov.ng/mps/single/566"
+},
+{
+id: 34,
+name: "Sen. Mohammad Mainasara Aliero",
+position: "Senator",
+department: "Kebbi Central",
+phone: "+2347066847000",
+email: "senatoraliero@yahoo.com",
+office: "No 14 Ibrahim Badamasi Road B/Kebbi",
+state: "kebbi",
+socials: [],
+source: "https://nass.gov.ng/mps/single/411"
+},
+{
+id: 35,
+name: "Sen. Yahaya Abdullahi",
+position: "Senator",
+department: "Kebbi North",
+phone: "",
+email: "yahaya.abdullahi@nass.gov.ng",
+office: "",
+state: "kebbi",
+socials: [
+{ platform: "facebook", url: "https://www.facebook.com/senatorabdullahiyahaya/" }
+],
+source: "https://nass.gov.ng/mps/single/452"
+},
+{
+id: 36,
+name: "Sen. Akpoti-Uduaghan Natasha",
+position: "Senator",
+department: "Kogi Central",
+phone: "+2348033112104",
+email: "nakpoti@hotmail.com",
+office: "Opp. Central Mosque, Okene, Kogi State",
+state: "kogi",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/natashaakpoti" },
+{ platform: "twitter", url: "https://x.com/NatashaAkpoti/" },
+{ platform: "facebook", url: "https://www.facebook.com/natashaakpoti1/" }
+],
+source: "https://nass.gov.ng/mps/single/624"
+},
+{
+id: 37,
+name: "Sen. Jibrin Isah",
+position: "Senator",
+department: "Kogi East",
+phone: "+2348185651909",
+email: "isahj@ymail.com",
+office: "Old Egume Road Ayangba, Kogi State",
+state: "kogi",
+socials: [
+{ platform: "instagram", url: "https://www.instagram.com/senator_jie" },
+{ platform: "twitter", url: "https://x.com/Senator_JIE" },
+{ platform: "facebook", url: "https://www.facebook.com/p/Sen-Isah-Jibrin-Echocho-100080672552507/" }
+],
+source: "https://nass.gov.ng/mps/single/101"
+}
+];
